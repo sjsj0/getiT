f4d7fdc343bb2215f27545a00b171b7e3ddd7df3
piyushkaushik17 <kaushikpiyush17@gmail.com>
2021-07-16 23:36:52 +0530

    added basic card

--- frontend/getit_fe/src/components/App.js
+++ frontend/getit_fe/src/components/App.js
@@ -3,15 +3,23 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Searchbar from "./Searchbar";
 import Slides from "./Slides"
+import ItemCard from "./cards";
+import './card_container.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Searchbar />
-      <Slides />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Searchbar/>
+            <Slides/>
+            <div className="card_container">
+                <ItemCard/>
+                <ItemCard/>
+                <ItemCard/>
+
+            </div>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
