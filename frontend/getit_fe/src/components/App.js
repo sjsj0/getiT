7238cce9b4a923f45790e0bd299cd3be497fb1c9
Shubham Dhiman <shubhamdhiman8.sd@gmail.com>
2021-07-16 20:21:49 +0530

    Basic Carousel Added

--- frontend/getit_fe/src/components/App.js
+++ frontend/getit_fe/src/components/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from "./Header";
 import Footer from "./Footer";
 import Searchbar from "./Searchbar";
+import Slides from "./Slides"
 
 function App() {
   return (
     <div className="App">
       {/* <Header /> */}
       <Searchbar />
+      <Slides />
       <Footer />
     </div>
   );
